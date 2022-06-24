06910a53d2f4a26020834543a2a85cfa12199d3a
Ed-Neema <ednahakoth21@gmail.com>
2022-06-24 15:03:45 +0200

    about lifecycle hooks and SFC in general

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Twotter"/>
+  <div id="app">
+    {{ user.username }} - {{ fullName }}
+
+    <strong>Followers: </strong> {{ followers }}
+    <button v-on:click="followUser()">Follow</button>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: '@_edmucious',
+        firstName: "Ednah",
+        lastName: "Akoth",
+        isAdmin: true
+      }
+    }
+  },
+
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+
+  methods: {
+
+    followUser() {
+      this.followers++
+    },
+    
+
+  },
+  mounted() {
+    this.followUser();
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount){
+       if(oldFollowerCount < newFollowerCount){
+        console.log(`${this.user.username} has gained a follower`)
+       }
+    }
+
+  },
+
+
   components: {
-    HelloWorld
+
   }
 }
+
 </script>
 
 <style>
@@ -19,8 +66,21 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+}
+
+button {
+  cursor: pointer;
+  display: inline-block;
+  background: #333;
+  color: white;
+  font-size: 18px;
+  border: 0;
+  padding: 1rem 1.5rem;
 }
 </style>
